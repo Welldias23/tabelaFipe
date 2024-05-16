54b6aaca14ac922895e1adb1f44020a5a3e966da
Wellington Dias <wellingtond712@gmail.com>
2024-05-16 16:39:15 -0300

    Projeto finalizado

--- src/main/java/br/com/welldias/tabelaFipe/principal/Principal.java
+++ src/main/java/br/com/welldias/tabelaFipe/principal/Principal.java
@@ -1,12 +1,16 @@
 package br.com.welldias.tabelaFipe.principal;
 
 import br.com.welldias.tabelaFipe.model.Dados;
+import br.com.welldias.tabelaFipe.model.Veiculo;
 import br.com.welldias.tabelaFipe.model.modelos;
 import br.com.welldias.tabelaFipe.service.ConumoApi;
 import br.com.welldias.tabelaFipe.service.Covertedados;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -57,5 +61,36 @@ public class Principal {
         modelosLista.modelos().stream()
                 .sorted(Comparator.comparing(Dados::codigo))
                 .forEach(System.out::println);
+
+        System.out.println("\nDigite um trexo do nome do veiculo a ser buscado:");
+
+        var nomeVeiculo = leitura.nextLine();
+
+        List<Dados> modelosFiltrados = modelosLista.modelos().stream()
+                .filter(m -> m.nome().toLowerCase().contains(nomeVeiculo.toLowerCase()))
+                .collect(Collectors.toList());
+
+        System.out.println("\nModelos filtrados:");
+        modelosFiltrados.forEach(System.out::println);
+
+        System.out.println("\nDigte por favor o codigo do modelo para buscar a avaliacao:");
+        var codigoModelo = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoModelo + "/anos";
+        json = conumoApi.obterdados(endereco);
+
+        List<Dados> anos = coversor.obterLista(json, Dados.class);
+        List<Veiculo> veiculos = new ArrayList<>();
+
+        for (int i = 0; i < anos.size(); i++) {
+            var enderecoAnos = endereco + "/" + anos.get(i).codigo();
+            json = conumoApi.obterdados(enderecoAnos);
+            Veiculo veiculo = coversor.obterDados(json, Veiculo.class);
+            veiculos.add(veiculo);
+        }
+
+        System.out.println("\nTodos os veiculos filtrados por an com avaliacoes:");
+        veiculos.forEach(System.out::println);
+
     }
 }
